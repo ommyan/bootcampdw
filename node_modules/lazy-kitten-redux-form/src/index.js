a8709dff08f8d0327674d91b4abe0b340b94fa57
yan kusyanto <ommyanu@gmail.com>
2018-05-08 14:44:38 +0700

    commit ulang tanpa node

--- node_modules/lazy-kitten-redux-form/src/index.js
+++
@@ -1,6 +0,0 @@
-export { default as LkTextInput } from './components/LkTextInput';
-export { default as LkPicker } from './components/LkPicker';
-export { default as LkRadioButton } from './components/LkRadioButton';
-export { default as LkCheckbox } from './components/LkCheckbox';
-
-
